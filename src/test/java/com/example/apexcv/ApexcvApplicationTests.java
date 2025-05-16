c702b336719da46c57686e566e59e04418c5aadb
thydynh03 <thindde180805@fpt.edu.vn>
2025-05-16 14:30:10 +0700

    base code

---
+++ src/test/java/com/example/apexcv/ApexcvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apexcv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApexcvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
