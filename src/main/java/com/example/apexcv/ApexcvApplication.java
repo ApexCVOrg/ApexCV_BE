f4bc5ccacd92a7dd829b6a03406f2e8457db913c
thydynh03 <thindde180805@fpt.edu.vn>
2025-05-16 14:30:10 +0700

    base code

---
+++ src/main/java/com/example/apexcv/ApexcvApplication.java
@@ -0,0 +1,13 @@
+package com.example.apexcv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApexcvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApexcvApplication.class, args);
+	}
+
+}
